b12bf108f5143bdd6f27dfb00fd1b4d976e207b9
Hiroshi Fujiwara <fujiwara@no1s.biz>
2020-01-14 10:13:38 +0000

    added js directory

---
+++ js/load.js
@@ -0,0 +1,28 @@
+// 画像読込のシーン
+var loadScene = new Phaser.Scene("loadScene");
+
+loadScene.preload = function() {
+    // スタート画像
+    this.load.image('gamestart', 'assets/images/gamestart.gif');
+    // ゲームオーバー画像
+    this.load.image('gameover', 'assets/images/gameover.png');
+    // マップデータ
+    this.load.tilemapTiledJSON('map', 'assets/data/map1.json');
+    // マップ画像
+    this.load.spritesheet('tiles', 'assets/images/tiles.png', {frameWidth: 70, frameHeight: 70});
+    // コイン画像
+    this.load.image('coin', 'assets/images/coin.png');
+    // プレイヤーデータ
+    this.load.atlas('player', 'assets/images/player1.png', 'assets/data/player.json');
+    // 敵画像
+    this.load.image('enemy01', 'assets/images/enemy01.png');
+    this.load.image('enemy02', 'assets/images/enemy02.png');
+    this.load.image('enemy05', 'assets/images/enemy05.png');
+    this.load.image('enemy07', 'assets/images/enemy07.png');
+    this.load.image('enemy08', 'assets/images/enemy08.png');
+};
+
+loadScene.create = function() {
+    // 読み込み完了後にstartSceneを起動
+    this.scene.start("startScene");
+};
