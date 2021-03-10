28ba6def58f7fb5282aefeae2c28f0e303cbabe9
EC2 Default User <hiroshi.829f@gmail.com>
2021-03-10 08:26:00 +0000

    fix main.js to fire

--- js/load.js
+++ js/load.js
@@ -13,13 +13,19 @@ loadScene.preload = function() {
     // コイン画像
     this.load.image('coin', 'assets/images/coin.png');
     // プレイヤーデータ
-    this.load.atlas('player', 'assets/images/player1.png', 'assets/data/player.json');
+    this.load.atlas('player1', 'assets/images/player1.png', 'assets/data/player.json');
+    this.load.atlas('player2', 'assets/images/player2.png', 'assets/data/player.json');
+    this.load.atlas('player3', 'assets/images/player3.png', 'assets/data/player.json');
+    this.load.atlas('player4', 'assets/images/player4.png', 'assets/data/player.json');
+    this.load.atlas('player5', 'assets/images/player5.png', 'assets/data/player.json');
     // 敵画像
     this.load.image('enemy01', 'assets/images/enemy01.png');
     this.load.image('enemy02', 'assets/images/enemy02.png');
     this.load.image('enemy05', 'assets/images/enemy05.png');
     this.load.image('enemy07', 'assets/images/enemy07.png');
     this.load.image('enemy08', 'assets/images/enemy08.png');
+    // ファイヤ画像
+    this.load.image('fire', 'assets/images/fire.png');
 };
 
 loadScene.create = function() {
